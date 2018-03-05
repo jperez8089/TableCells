354cc59a0fd73a5313cbaa5f465d917db500538b
Javier Perez Primary <javierperezprimary@javiers-macbook-pro.local>
2018-03-05 13:44:00 -0600

    initial

--- TableCells/ViewController.swift
+++ TableCells/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let cellTableIdentifier = "CellTableIdentifier"
+    @IBOutlet var tableView:UITableView!
+    let computers = [
+        ["name" : "MacBook Air", "Color" : "Silver" ],
+        ["name" : "Macbook Pro", "Color" : "Silver"],
+        ["name" : "iMac", "Color" : "Silver"],
+        ["name" : "Mac Mini", "Color" : "Silver"],
+        ["name" : "Mac Pro", "Color" : "Black"],
+        
+    ]
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.register(NameAndColorCell.self, forCellReuseIdentifier: cellTableIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return computers.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell( withIdentifier: cellTableIdentifier, for: indexPath)
+        as! NameAndColorCell
+        
+        let rowData = computers[indexPath.row]
+        cell.name = rowData["Name"]!
+        cell.color = rowData["Color"]!
+        
+        return cell
+    }
 }
 
